cf85b6e7d3a129ed3286e0fbb289f1e8327a90b0
ArturVesta <meirfons2@gmail.com>
2025-02-05 08:14:21 +0200

    Add files via upload

--- index_data/preview_png.js
+++ index_data/preview_png.js
@@ -1,12 +1,29 @@
 function showImage(imgs) {
-  var imagepreviewbase = document.getElementById("imagepreviewbase");
+  if(window.location.href === "https://arturvesta.github.io/SMC_Pack/index_wallpapers.html") {
+    var location = "https://arturvesta.github.io/SMC_Pack/index_data/Wallpapers/";
+  }else{// Location for testing the script ! cannot be show for all.
+    var location = "file:///C:/Profiles/MNTS/WorkSpacesHTML/SMCPack/index_data/Wallpapers/";
+  }
+  var format = ".png";
   ex2.src = imgs.src;
   imagepreviewbase.style.left = "0%";
   document.body.style.overflowY = "hidden";
+  document.getElementById("counts").innerHTML = imgs.alt;
   setTimeout(() => {
     imagepreviewbase.style.backgroundColor = "var(--opacity-color)";
     imagepreviewbase.style.backdropFilter = "blur(5px)";
   },340);
+  for(var i = 0; i < listWallpapers.length; i++) {
+    if(ex2.src === location + listWallpapers[0] + format) {
+      document.getElementById("prevBtn").style.display = "none";
+    }else
+    if(ex2.src === location + listWallpapers[210] + format) {
+      document.getElementById("nextBtn").style.display = "none";
+    }else{
+      document.getElementById("prevBtn").style.display = "block";
+      document.getElementById("nextBtn").style.display = "block";
+    };
+  }
 };
 function closeImgPreview() {
   var imagepreviewbase = document.getElementById("imagepreviewbase");
---
+++ index_data/wallpapers_perviose.js
@@ -0,0 +1,48 @@
+function wallBack() {
+  let count = document.getElementById("counts").innerHTML;
+  if(window.location.href === "https://arturvesta.github.io/SMC_Pack/index_wallpapers.html") {
+    var location = "https://arturvesta.github.io/SMC_Pack/index_data/Wallpapers/";
+  }else{// Location for testing the script ! cannot be show for all.
+    var location = "file:///C:/Profiles/MNTS/WorkSpacesHTML/SMCPack/index_data/Wallpapers/";
+  }
+  var format = ".png";
+  document.getElementById("counts").innerHTML = --count;
+  for(var i = 0; i < listWallpapers.length; i++) {
+    if(ex2.src === location + listWallpapers[i] + format) {
+      ex2.src = location + listWallpapers[i - 1] + format;
+    };
+    if(ex2.src === location + listWallpapers[0] + format) {
+      document.getElementById("prevBtn").style.display = "none";
+    }else
+    if(ex2.src === location + listWallpapers[210] + format) {
+      document.getElementById("nextBtn").style.display = "none";
+    }else{
+      document.getElementById("prevBtn").style.display = "block";
+      document.getElementById("nextBtn").style.display = "block";
+    };
+  }
+};
+function wallNext() {
+  let count = document.getElementById("counts").innerHTML;
+  if(window.location.href === "https://arturvesta.github.io/SMC_Pack/index_wallpapers.html") {
+    var location = "https://arturvesta.github.io/SMC_Pack/index_data/Wallpapers/";
+  }else{// Location for testing the script ! cannot be show for all.
+    var location = "file:///C:/Profiles/MNTS/WorkSpacesHTML/SMCPack/index_data/Wallpapers/";
+  }
+  var format = ".png";
+  document.getElementById("counts").innerHTML = ++count;
+  for(var i = 0; i < listWallpapers.length; i++) {
+    if(ex2.src === location + listWallpapers[i] + format) {
+      ex2.src = location + listWallpapers[count] + format;
+    };
+    if(ex2.src === location + listWallpapers[0] + format) {
+      document.getElementById("prevBtn").style.display = "none";
+    }else
+    if(ex2.src === location + listWallpapers[210] + format) {
+      document.getElementById("nextBtn").style.display = "none";
+    }else{
+      document.getElementById("prevBtn").style.display = "block";
+      document.getElementById("nextBtn").style.display = "block";
+    };
+  }
+};
